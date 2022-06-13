0066d5a2c54e8d17a77b67f8d9ad2a9839fe4106
Tea <icepie.dev@gmail.com>
2022-06-13 14:27:13 +0800

    components: init

---
+++ src/components/Hello.vue
@@ -0,0 +1,5 @@
+<script lang="ts" setup></script>
+
+<template>
+  <text>Hi</text>
+</template>
--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -3,17 +3,28 @@
     class="index bg-white dark:bg-black h-screen pl-7.5"
     :class="{ 'text-[50px]': true }"
   >
+    <hello />
     <text class="text-red-400 dark:text-green-400">{{ title }}</text>
   </view>
 </template>
 
-<script setup lang="ts">
+<script lang="ts" setup>
 import { onShow } from "@dcloudio/uni-app";
 import { ref } from "vue";
-const title = ref("Hello world!");
+import hello from "@/components/Hello.vue";
+// import { CSwiper } from "@/components/c-swiper/c-swiper.vue"; // 导入组件
+
+let title = ref("Hello world!");
 
 onShow(async () => {
   console.log("App Show");
+
+  uni.showToast({
+    title: "测试弹窗",
+    icon: "none",
+    duration: 2000,
+    // mask: true,
+  });
 });
 </script>
 
