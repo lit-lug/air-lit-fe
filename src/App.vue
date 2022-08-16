d61fe1a5878cfdaaf1c6d7cccc7af5d25c00a48f
Tea <icepie.dev@gmail.com>
2022-08-16 14:25:25 +0800

    clean useless code

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,16 @@
 <script lang="ts" setup>
 import { onLaunch, onShow, onHide, onThemeChange } from "@dcloudio/uni-app";
 import { useTmpiniaStore } from "@/tmui/tool/lib/tmpinia";
-import { useAppStore } from "./store/app";
-import { storeToRefs } from "pinia";
 
 const tmStore = useTmpiniaStore();
-
-const { statusBarHeight, menuButtonBounding } = storeToRefs(useAppStore());
 
 const systemInfo: any = uni.getSystemInfoSync();
 
 onLaunch(() => {
-  // #ifdef MP-WEIXIN || MP-QQ
-  // the systemInfo.theme is only support dark mode in WeChat and QQ
-  statusBarHeight.value = systemInfo!.statusBarHeight || 44;
-  menuButtonBounding.value = uni.getMenuButtonBoundingClientRect();
-  // uni.onThemeChange(
-  //   (res: UniApp.OnThemeChangeCallbackResult) => (darkMode.value = res.theme === "dark")
-  // );
-  // #endif
-
-  // #ifdef H5
-
   // 隐藏原生标题栏
   uni.hideTabBar({});
+
+  // #ifdef H5
 
   // 页面大小改变刷新页面
   window.addEventListener("resize", () => {
@@ -36,19 +23,7 @@ onLaunch(() => {
   colorScheme.addEventListener("change", (e: MediaQueryListEvent) =>
     tmStore.setTmVuetifyDark(e.matches)
   );
-  // The data is obtained from iPhone13 miniprogram but statusBarHeight, top and bottom values are subtracted from the statusBarHeight value
-  statusBarHeight.value = 0;
-  menuButtonBounding.value = {
-    width: 87,
-    height: 32,
-    left: 281,
-    top: 4,
-    right: 368,
-    bottom: 56,
-  };
   // #endif
-
-  console.log(statusBarHeight.value, menuButtonBounding.value);
 
   /**
    * 设置主题，用户配置优先
--- src/store/app.ts
+++ src/store/app.ts
@@ -2,30 +2,30 @@ import { pinia } from '@/modules/pinia'
 import { defineStore } from 'pinia'
 import { computed, ref } from 'vue'
 
-interface MenuButtonBoundingClientRect {
-    width: number
-    height: number
-    top: number
-    left: number
-    right: number
-    bottom: number
-}
+// interface MenuButtonBoundingClientRect {
+//     width: number
+//     height: number
+//     top: number
+//     left: number
+//     right: number
+//     bottom: number
+// }
 
 export const useAppStore = defineStore(
     'app',
     () => {
-        const statusBarHeight = ref(0)
-        const menuButtonBounding = ref<MenuButtonBoundingClientRect>()
-        const customBarHeight = computed(
-            () => !menuButtonBounding.value
-                ? 0
-                : menuButtonBounding.value.bottom + menuButtonBounding.value.top - statusBarHeight.value)
+        // const statusBarHeight = ref(0)
+        // const menuButtonBounding = ref<MenuButtonBoundingClientRect>()
+        // const customBarHeight = computed(
+        //     () => !menuButtonBounding.value
+        //         ? 0
+        //         : menuButtonBounding.value.bottom + menuButtonBounding.value.top - statusBarHeight.value)
 
-        return {
-            statusBarHeight,
-            customBarHeight,
-            menuButtonBounding,
-        }
+        // return {
+        //     statusBarHeight,
+        //     customBarHeight,
+        //     menuButtonBounding,
+        // }
     })
 
 // Need to be used outside the setup
