43bfd17bad3f92b6aeec433fa28938d907529db9
Tea <icepie.dev@gmail.com>
2022-11-12 01:03:52 +0800

    tab

---
+++ src/components/UTab/UTab.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts"></script>
+
+<template class="sticky">
+  <div class="flex flex-col b-b-blue b-b-2">
+    <div class="flex flex-row justify-around items-center px-2 my-2">
+      <div w-full justify-center flex flex-col items-center py-2>
+        test1
+        <div class="w-full h-1 bg-blue-6/80"></div>
+      </div>
+
+      <div w-full justify-center flex flex-col items-center py-2>
+        test2
+        <!-- <div class="w-full h-1 bg-blue-5"></div> -->
+      </div>
+    </div>
+
+    <!-- 蓝色下滑线 -->
+  </div>
+</template>
--- src/pages/index/elec/elec.vue
+++ src/pages/index/elec/elec.vue
@@ -1,46 +1,15 @@
 <script setup lang="ts">
-import { GetCalendar } from "~/common/api";
-
-const { getYearTerm } = useCourseStore();
-
-const imgUrl = ref("");
-
-const previewImg = () => {
-  uni.previewImage({
-    urls: [imgUrl.value],
-  });
-};
-
-const yearTerm = getYearTerm();
-
-const year = ref(yearTerm.year);
-
-const term = ref(yearTerm.term);
-
-const subTitle = ref("");
-
-onReady(async () => {
-  const { data: calendar } = await GetCalendar();
-  if (calendar) {
-    subTitle.value = calendar.sub_title;
-    imgUrl.value = calendar.img_url;
-  }
-});
-
-onPullDownRefresh(async () => {
-  const { data: calendar } = await GetCalendar();
-  if (calendar) {
-    subTitle.value = calendar.sub_title;
-    imgUrl.value = calendar.img_url;
-  }
-  uni.stopPullDownRefresh();
-});
+import UTab from "~/components/UTab/UTab.vue";
 </script>
 
 <template>
   <UBasePage>
     <!-- 标题栏 -->
-    <template v-slot:navContent>宿舍用电</template>
+    <UTab></UTab>
+
+    <template v-slot:navContent :scroll="false" :tabs="['全部', '进行中', '已完成']"
+      >宿舍用电</template
+    >
 
     <UResult></UResult>
   </UBasePage>
