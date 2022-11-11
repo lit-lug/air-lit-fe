693e63e79d2ca14c4138bf9d20fde54f9024eb03
Tea <icepie.dev@gmail.com>
2022-11-12 01:11:52 +0800

    tab: test SelectorQuery()

--- src/components/UTab/UTab.vue
+++ src/components/UTab/UTab.vue
@@ -1,19 +1,32 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+onMounted(() => {
+  console.log("UInput mounted");
+  const currentInstance = getCurrentInstance();
+  const q = uni.createSelectorQuery().in(currentInstance);
+  q.select("#scrollContainer")
+    .boundingClientRect((res) => {
+      console.log(res);
+    })
+    .exec();
+});
+</script>
 
 <template class="sticky">
-  <div class="flex flex-col b-b-blue b-b-2">
-    <div class="flex flex-row justify-around items-center px-2 my-2">
-      <div w-full justify-center flex flex-col items-center py-2>
-        test1
-        <div class="w-full h-1 bg-blue-6/80"></div>
+  <scroll-view id="scrollContainer" :style="{ position: 'fixed', zIndex: 1993 }">
+    <div class="flex flex-col b-b-blue b-b-2">
+      <div class="flex flex-row justify-around items-center px-2 my-2">
+        <div w-full justify-center flex flex-col items-center py-2>
+          test1
+          <div class="w-full h-1 bg-blue-6/80"></div>
+        </div>
+
+        <div w-full justify-center flex flex-col items-center py-2>
+          test2
+          <!-- <div class="w-full h-1 bg-blue-5"></div> -->
+        </div>
       </div>
 
-      <div w-full justify-center flex flex-col items-center py-2>
-        test2
-        <!-- <div class="w-full h-1 bg-blue-5"></div> -->
-      </div>
+      <!-- 蓝色下滑线 -->
     </div>
-
-    <!-- 蓝色下滑线 -->
-  </div>
+  </scroll-view>
 </template>
