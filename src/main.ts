2387a3af5d89cc0f0d68d26031c4d6bdce842a1f
Tea <icepie.dev@gmail.com>
2023-01-23 14:01:24 +0800

    deps: update

--- src/main.ts
+++ src/main.ts
@@ -1,27 +1,11 @@
 import { createSSRApp } from 'vue'
 import App from './App.vue'
-
-
 import { setupPinia } from '~/modules/pinia'
 
 import 'uno.css'
 
-// // #ifdef H5
-// import { registerSW } from 'virtual:pwa-register'
-
-// registerSW({
-//   // 每小时检查一次
-//   onRegistered: r => r && setInterval(async () => {
-//     await r.update()
-//   }, 1000 * 60 * 60),
-//   // 注册失败则报错到 console
-//   onRegisterError: error => console.error(error)
-// })
-// // #endif
-
 export function createApp() {
   const app = createSSRApp(App)
-
   // Configure store
   // https://pinia.vuejs.org/
   setupPinia(app)
