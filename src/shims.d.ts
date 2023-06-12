01b6572c63bda6c01a8e1242f9a25171277c4008
Tea <icepie.dev@gmail.com>
2023-06-12 22:32:55 +0800

    up

--- src/shims.d.ts
+++ src/shims.d.ts
@@ -2,11 +2,11 @@
 /// <reference types="vite/client" />
 
 
-// declare module '*.vue' {
-//   import type { DefineComponent } from 'vue'
-//   const component: DefineComponent<{}, {}, any>
-//   export default component
-// }
+declare module '*.vue' {
+  import type { DefineComponent } from 'vue'
+  const component: DefineComponent<{}, {}, any>
+  export default component
+}
 
 declare module '*.md' {
   const content: string
